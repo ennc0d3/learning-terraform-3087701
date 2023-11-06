97518dfa96ae64799105914aeae59c6aaf0ad42d
Nagarajan Ganesan <ennmail@gmail.com>
2023-11-06 19:26:28 +0000

    Fix ingress rules(3)

--- main.tf
+++ main.tf
@@ -24,11 +24,15 @@ module "web-sg-group" {
   ingress_rules = ["http-80-tcp", "https-443-tcp"]
   name          = "web-sg-group"
   vpc_id        = data.aws_vpc.default.id
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
+
   tags = {
     "name" = "http-and-https"
   }
 }
 resource "aws_instance" "web" {
+  name = "web-instance"
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
   availability_zone      = var.availability_zone
